5fe0615675ef41391689ced8b7f4c59039ca2037
Victor Oliveira <victor.oliveira@arizona.global>
2021-05-12 17:43:06 -0300

    UseState through function

--- plot-chart/src/components/pages/home.jsx
+++ plot-chart/src/components/pages/home.jsx
@@ -1,39 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Top from './top'
 import ViewData from './viewData'
 import Button from '@material-ui/core/Button';
 import '../css/home.css'
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.generateChart = this.generateChart.bind(this);
-        this.state = {
-            chart: false
-        }
-    }
-    generateChart() {
-        this.setState({
-            chart: true
-        });
+function Home() {
+
+    const [generateChart, setGenerateChart] = useState(false)
+
+    function showgenerateChart() {
+
+        const show = setGenerateChart(!generateChart)
+        return (show)
+
     }
 
-    render() {
-        return (
+    return (
+        <div>
+            <Top></Top>
             <div>
-                <Top></Top>
-                <div>
-                    {this.state.chart === true ? <ViewData /> : ''}
-
-                </div>
-                <section className='ButtonGerateChart'>
-                
-                <Button variant="contained" color="primary" onClick={this.generateChart} > GENERATE CHART </Button>
-                </section>
+                {generateChart === true ? <ViewData /> : ''}
 
             </div>
-        )
-    }
+            <section className='ButtonGerateChart'>
+
+                <Button variant="contained" color="primary" onClick={showgenerateChart} > GENERATE CHART </Button>
+            </section>
+
+        </div>
+    )
 }
 
 export default Home
